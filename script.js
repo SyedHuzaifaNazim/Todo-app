2436386c875aed158e5bdd3273f0952b5202c7e0
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2025-02-06 21:27:33 +0500

    after along time , little bit changes ...

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ function addTask() {
 	let task = input.value;
 	if (task === "") {
 		alert("Please enter a task.");
+		return;
 	} else {
 		let li = document.createElement("li");
 		li.innerHTML = task + "<button class='delete' onclick='deleteTask(this)'>Delete</button>";
 		list.appendChild(li);
 		input.value = "";
+		input.focus();
 	}
 }
+function addTaskEnter(event) {
+	if (event.keyCode === 13) {
+		addTask();
+	}
+}
+input.addEventListener("keydown", addTaskEnter);
 
 function deleteTask(element) {
 	element.parentElement.remove();
